9f65b42d12642ad270ce18ca7876d23465ab7393
boniface <boniface@kabaso.com>
2019-02-02 13:49:32 +0200

    updated

--- concurrency/main.go
+++ concurrency/main.go
@@ -1,1 +1,25 @@
-package concurrency
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		fmt.Println(" First Goroutine is Sleeping ")
+		time.Sleep(1)
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		fmt.Println(" Second goroutine is sleeping ")
+	}()
+	wg.Wait()
+	fmt.Println("All goroutines complete")
+}
