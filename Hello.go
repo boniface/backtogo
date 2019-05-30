d89faea4c340cfc273c84983a9d76081bd7a8439
boniface <boniface@kabaso.com>
2019-05-30 19:11:39 +0200

    updated

--- Hello.go
+++ Hello.go
@@ -1,14 +1,30 @@
 package main
 
-
+import "fmt"
 
 func main() {
-	number:=5
-	println("The Number is ", number)
-	println("The Point", &number)
+	data := []int{9, 3, 6, 7, 8, 2, 1, 4}
+	BubbleSort(data, less)
+	fmt.Println(" the Sorted Data is ", data)
 
 }
 
+func less(value1 int, value2 int) bool {
+	return value1 < value2
+}
 
+func more(value1 int, value2 int) bool {
+	return value1 > value2
+}
 
-
+func BubbleSort(arr []int, comp func(int, int) bool) {
+	size := len(arr)
+	for i := 0; i < (size - 1); i++ {
+		for j := 0; j < size-i-1; j++ {
+			if comp(arr[j], arr[j+1]) {
+				//swapping
+				arr[j+1], arr[j] = arr[j], arr[j+1]
+			}
+		}
+	}
+}
--- datastructures/linkedlists/singly.go
+++ datastructures/linkedlists/singly.go
@@ -1,11 +1,87 @@
-package linkedlists
+package main
 
-type ListNode struct {
-	next *ListNode
+import "fmt"
+
+type Node struct {
+	next *Node
 	data interface{}
 }
 
-type LinkedList struct {
-	head *ListNode
+type List struct {
+	head *Node
 	size int
+}
+
+func (list *List) Size() int {
+	return list.size
+}
+
+func (list *List) isEmpty() bool {
+	return list.size == 0
+}
+
+func (list *List) Addhead(value interface{}) {
+	list.head = &Node{list.head, value}
+	list.size++
+}
+
+func (list *List) addTail(value int) {
+	curr := list.head
+	newNode := &Node{nil, value}
+
+	if curr == nil {
+		list.head = newNode
+		return
+	}
+
+	for curr.next != nil {
+		curr = curr.next
+	}
+
+	curr.next = newNode
+
+}
+
+func (list *List) SortedInsert(value int) {
+	newNode := &Node{nil, value}
+	curr := list.head
+
+	if curr == nil || curr.data.(int) < value {
+		newNode.next = list.head
+		list.head = newNode
+		return
+	}
+
+}
+
+func (list *List) Print() {
+	temp := list.head
+
+	for temp != nil {
+		fmt.Println("The Data is ", temp.data)
+		temp = temp.next
+	}
+	fmt.Println(" This is a Test ")
+
+}
+
+func main() {
+
+	fmt.Println(" Adding to the Head")
+	lst := new(List)
+
+	lst.Addhead(1)
+	lst.Addhead(2)
+	lst.Addhead(3)
+	lst.Addhead(4)
+	lst.Print()
+
+	tail := &List{}
+
+	tail.addTail(1)
+	tail.addTail(2)
+	tail.addTail(3)
+	tail.addTail(4)
+	tail.Print()
+
 }
